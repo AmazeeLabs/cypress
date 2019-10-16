2392ab5592da6f7d649cc8ced44fcb70e1fcbc69
Philipp Melab <philipp.melab@gmail.com>
2019-10-16 16:13:33 +0200

    Read suites from environment.

--- tests/Kernel/CypressPathsTest.php
+++ tests/Kernel/CypressPathsTest.php
@@ -28,13 +28,11 @@ class CypressPathsTest extends KernelTestBase {
   }
 
   public function testCypressTestDirectories() {
-    $_ENV[CypressTestDirectoriesFactory::CYPRESS_SUITE_PREFIX . 'FEATURES'] = 'features';
     $appRoot = $this->container->get('app.root');
     $modulePath = drupal_get_path('module', 'cypress');
     $this->assertEquals(
       [
         'cypress' => $appRoot . '/' . $modulePath . '/' . CypressTestDirectoriesFactory::CYPRESS_TEST_DIRECTORY,
-        'features' => $appRoot . '/features',
       ],
       $this->container->get('cypress.test_directories')
     );
